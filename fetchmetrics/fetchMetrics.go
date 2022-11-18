d8dce7a6a353623607125172aabbba13288388c4
Shrinidhi P <srinidhip1700@gmail.com>
2022-11-18 08:49:10 +0530

    Updated Fetch metrics code with opensearch client

---
+++ fetchmetrics/fetchMetrics.go
@@ -0,0 +1,46 @@
+package fetchmetrics
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	elasticsearch "github.com/opensearch-project/opensearch-go"
+	utils "fetchMetrics/utils"
+)
+
+//Descriptions: Fetch metrics will create an opensearch client and fetches the node and cluster level details and 
+//indexes them into elasticesearch. It also deletes documents that are older than 72 hours
+func FetchMetrics() {
+	ctx := context.Background()
+
+	//create a configuration that is to be passed while creating the client 
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+		 Username: "elastic",
+		 Password: "Zuf4RkyeuO9ANJBSY0yQ",
+	}
+
+	//create the client using the configuration
+	esClient, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		fmt.Println("Elasticsearch connection error:", err)
+	}
+	res, err := esClient.Info()
+	if err != nil {
+		log.Fatalf("client.Info() ERROR:", err)
+	}
+	fmt.Println("Response: ", res)
+
+	//check if current node is the master node and update the cluster stats if it is master 
+	if utils.CheckIfMaster(esClient, ctx) {  //need to update check if master with opensearch-go package
+		fmt.Println("It's the master node")
+		IndexClusterHealth(esClient, ctx)
+	}
+	//Index the the node stats
+	IndexNodeStats(esClient, ctx)
+	//Purge documents from elasticsearch index that are older than 72 hours
+	DeleteOldDocs(esClient,ctx) //need to update purge old documents with opensearch-go package
+}
---
+++ fetchmetrics/purgeOldDocs.go
@@ -0,0 +1,36 @@
+package fetchmetrics
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	elasticsearch "github.com/opensearch-project/opensearch-go"
+	esapi "github.com/opensearch-project/opensearch-go/opensearchapi"
+	utils "fetchMetrics/utils"
+)
+
+func DeleteOldDocs(esClient *elasticsearch.Client, ctx context.Context) {
+	var jsonQuery = []byte(`{
+		"query": {
+		  "bool": {
+			  "filter": {
+				  "range": {
+						  "Timestamp": {
+								  "lte":"now-72h"
+						  }
+					  }
+				  }
+			  }
+		  }
+	  }`)
+	  var indexName = []string{utils.IndexName}
+		  deleteByQueryRequest,err:=esapi.DeleteByQueryRequest{
+				Index: indexName,
+				Body: bytes.NewReader(jsonQuery),
+		  }.Do(ctx,esClient)
+		 if err!=nil{
+			log.Fatal("Unable to execute request: ",err)
+		 }
+		 fmt.Println("Document deleted: ",deleteByQueryRequest)
+}
