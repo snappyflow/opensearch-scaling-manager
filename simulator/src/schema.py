f201b930d992be3b2e12d83eb8a7088616b97795
Manoj Kumar Chandru <Manojkumar.Chandru@maplelabs.com>
2022-12-16 13:42:12 +0530

    Fix indentations

--- simulator/src/schema.py
+++ simulator/src/schema.py
@@ -1,106 +1,49 @@
 {
- 'cluster_name':{
-	'required':True,
-	'type':'string'
-	},
- 'cluster_hostname':{
-	'required':True,
-	'type':'string'
-	},
- 'cluster_ip_address':{
-	'required':True,
-	'type':'string'
-	},
- 'node_machine_type_identifier':{
-	'required':True,
-	'type':'string'
-	},
- 'total_nodes_count':{
-	'required':True,
-	'type':'number'
-	},
- 'data_nodes_count':{
-	'required':True,
-	'type':'number'
-	},
- 'master_eligible_nodes_count':{
-	'required':True,
-	'type':'number'
-	},
- 'index_count':{
-	'required':True,
-	'type':'number'
-	},
- 'shards_per_index':{
-	'required':True,
-	'type':'number'
-	},
-'index_roll_over_size_gb':{
-	'required':True,
-	'type':'number'
-	},
-'index_clean_up_age_days':{
-	'required':True,
-	'type':'number'
-	},
-'data_generation_interval_minutes':{
-	'required':True,
-	'type':'number'
-	},
-'data_ingestion':{
-	'required':False,
-	'type': 'dict',
-	'schema': {
-	  'states': {
-		'required':True,
-		'type':'list',
-        'schema': {
-          'type': 'dict',
-          'schema':{
-		    'position':{
-		      'required':True,
-			  'type':'number'
-			},
-			'time_hh_mm_ss':{
-			  'required':True,
-			  'type':'string'
-			},
-			'ingestion_rate_gb_per_hr':{
-			  'required':True,
-			  'type':'number'
-			}
-		  }
-      }
+    "cluster_name": {"required": True, "type": "string"},
+    "cluster_hostname": {"required": True, "type": "string"},
+    "cluster_ip_address": {"required": True, "type": "string"},
+    "node_machine_type_identifier": {"required": True, "type": "string"},
+    "total_nodes_count": {"required": True, "type": "number"},
+    "data_nodes_count": {"required": True, "type": "number"},
+    "master_eligible_nodes_count": {"required": True, "type": "number"},
+    "index_count": {"required": True, "type": "number"},
+    "shards_per_index": {"required": True, "type": "number"},
+    "index_roll_over_size_gb": {"required": True, "type": "number"},
+    "index_clean_up_age_days": {"required": True, "type": "number"},
+    "data_generation_interval_minutes": {"required": True, "type": "number"},
+    "data_ingestion": {
+        "required": False,
+        "type": "dict",
+        "schema": {
+            "states": {
+                "required": True,
+                "type": "list",
+                "schema": {
+                    "type": "dict",
+                    "schema": {
+                        "position": {"required": True, "type": "number"},
+                        "time_hh_mm_ss": {"required": True, "type": "string"},
+                        "ingestion_rate_gb_per_hr": {
+                            "required": True,
+                            "type": "number",
+                        },
+                    },
+                },
+            },
+            "randomness_percentage": {"required": True, "type": "number"},
+        },
     },
-	   'randomness_percentage':{
-				'required':True,
-				'type':'number'
-			}
-		}
-},
-'searches':{
-    'required':False,
-    'type':'list',
-    'schema': {
-    'type':'dict',
-    'schema':{
-	   'type':{
-	     'required':True,
-		 'type':'string'
-		},
-		'probability':{
-		  'required':True,
-		  'type':'number'
-		},
-		'cpu_load_percent':{
-		  'required':True,
-		  'type':'number'
-		},
-        'memory_load_percent':{
-		  'required':True,
-		  'type':'number'
-		}
-	}
-}
-}
+    "searches": {
+        "required": False,
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "type": {"required": True, "type": "string"},
+                "probability": {"required": True, "type": "number"},
+                "cpu_load_percent": {"required": True, "type": "number"},
+                "memory_load_percent": {"required": True, "type": "number"},
+            },
+        },
+    },
 }
