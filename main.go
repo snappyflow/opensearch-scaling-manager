c911a60d7ed4022553e60d6a036356ba19519c7a
Akhil-Nair-ML <akhil.nair@maplelabs.com>
2022-12-21 18:14:45 +0530

    [recommendation_validation]
    Added PollingInterval variable in a centralized place to have a easier control.

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func init() {
 
 func main() {
 	// The polling interval is set to 5 minutes and can be configured.
-	ticker := time.Tick(5 * time.Second)
+	ticker := time.Tick(time.Duration(config.PollingInterval) * time.Second)
 	for range ticker {
 		// The recommendation and provisioning should only happen on master node.
 		if cluster.CheckIfMaster() {
