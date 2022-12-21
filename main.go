513ea73c9dc9709a8917f110bcb1f0d41574ff14
Akhil-Nair-ML <akhil.nair@maplelabs.com>
2022-12-21 18:32:09 +0530

    [initial_provisioner]
    Changed the logic of PollingInterval in main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"scaling_manager/cluster"
 	"scaling_manager/config"
+	"scaling_manager/logger"
 	"scaling_manager/provision"
 	"scaling_manager/task"
-	"scaling_manager/logger"
 	"strings"
 	"time"
 )
@@ -22,7 +22,7 @@ func main() {
 	// A periodic check if there is a change in master node to pick up incomplete provisioning
 	go periodicProvisionCheck()
 	// The polling interval is set to 5 minutes and can be configured.
-	ticker := time.Tick(5 * time.Second)
+	ticker := time.Tick(time.Duration(config.PollingInterval) * time.Second)
 	for range ticker {
 		// This function is responsible for fetching the metrics and pushing it to the index.
 		// In starting we will call simulator to provide this details with current timestamp.
@@ -82,4 +82,3 @@ func periodicProvisionCheck() {
 		previousMaster = currentMaster
 	}
 }
-
