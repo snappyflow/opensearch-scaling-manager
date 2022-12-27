c0fc32aabb34b34a158d032b13c17001fcd28fc1
Shrinidhi-P-ML <Shrinidhi.P@maplelabs.com>
2022-12-27 19:15:09 +0530

    Simulated Disk space due to Data Ingestion

---
+++ simulator/src/constants.py
@@ -0,0 +1,62 @@
+# file paths
+SCHEMA_FILE_NAME = 'schema.py'
+CONFIG_FILE_PATH = 'config.yaml'
+PROVISION_LOCK_FILE_NAME = 'provision.lock'
+SIMULATION_GRAPH_FILE_NAME = 'simulation_diagram.png'
+
+# config keys
+DATA_INGESTION = "data_ingestion"
+DATA_INGESTION_STATES = 'states'
+SEARCHES = "searches"
+SIMULATION_FREQUENCY_MINUTES = "simulation_frequency_minutes"
+DATA_INGESTION_RANDOMNESS_PERCENTAGE = 'randomness_percentage'
+
+
+CLUSTER_STATE_GREEN = 'green'
+CLUSTER_STATE_YELLOW = 'yellow'
+CLUSTER_STATE_RED = 'red'
+
+CLUSTER_STATES = [CLUSTER_STATE_GREEN, CLUSTER_STATE_YELLOW, CLUSTER_STATE_RED]
+
+HIGH_INGESTION_RATE_GB_PER_HOUR = 60
+
+# mapping inputs for API endpoints
+CLUSTER_STATE = 'status'
+CPU_USAGE_PERCENT = 'cpu_usage_percent'
+DISK_USAGE_PERCENT = 'disk_usage_percent'
+MEMORY_USAGE_PERCENT = 'memory_usage_percent'
+TOTAL_NODES_COUNT = 'total_nodes_count'
+STAT_REQUEST = {
+    'cpu': CPU_USAGE_PERCENT,
+    'mem': MEMORY_USAGE_PERCENT,
+    'status': CLUSTER_STATE,
+    'nodes': TOTAL_NODES_COUNT,
+    'disk': DISK_USAGE_PERCENT
+}  
+
+CLUSTER_STATE = "status"
+TOTAL_NUM_NODES = "total_nodes_count"
+NUM_ACTIVE_SHARD_COUNT = "active_shards_count"
+NUM_ACTIVE_PRIMARY_SHARDS = "active_primary_shards"
+NUM_INITIALIZING_SHARDS = "initializing_shards_count"
+NUM_UNASSIGNED_SHARDS = "unassigned_shards_count"
+NUM_RELOCATING_SHARDS = "relocating_shards_count"
+NUM_MASTER_NODES = "master_eligible_nodes_count"
+NUM_ACTIVE_DATA_NODES = "active_data_nodes"
+STAT_REQUEST_CURRENT = {
+    "NumNodes": TOTAL_NUM_NODES,
+    "ClusterStatus": CLUSTER_STATE,
+    "NumActiveShards": NUM_ACTIVE_SHARD_COUNT,
+    "NumActivePrimaryShards": NUM_ACTIVE_PRIMARY_SHARDS,
+    "NumInitializingShards": NUM_INITIALIZING_SHARDS,
+    "NumUnassignedShards": NUM_UNASSIGNED_SHARDS,
+    "NumRelocatingShards": NUM_RELOCATING_SHARDS,
+    "NumMasterNodes": NUM_MASTER_NODES,
+    "NumActiveDataNodes": NUM_ACTIVE_DATA_NODES,
+}
+
+
+APP_PORT = 5000
+
+PRIMARY_SHARDS_IN_ROLLOVER_INDEX = 1
+REPLICA_SHARDS_IN_ROLLOVER_INDEX = 1
---
+++ simulator/src/schema.py
@@ -0,0 +1,51 @@
+{
+    "cluster_name": {"required": True, "type": "string"},
+    "cluster_hostname": {"required": True, "type": "string"},
+    "cluster_ip_address": {"required": True, "type": "string"},
+    "node_machine_type_identifier": {"required": True, "type": "string"},
+    "total_nodes_count": {"required": True, "type": "number"},
+    "active_data_nodes": {"required": True, "type": "number"},
+    "master_eligible_nodes_count": {"required": True, "type": "number"},
+    "index_count": {"required": True, "type": "number"},
+    "shards_per_index": {"required": True, "type": "number"},
+    "index_roll_over_size_gb": {"required": True, "type": "number"},
+    "index_clean_up_age_days": {"required": True, "type": "number"},
+    "replica_shard_count": {"required":True, "type": "number"},
+    "simulation_frequency_minutes": {"required": True, "type": "number"},
+    "total_disk_size_gb": {"required": True, "type": "number"},
+    "data_ingestion": {
+        "required": False,
+        "type": "dict",
+        "schema": {
+            "states": {
+                "required": True,
+                "type": "list",
+                "schema": {
+                    "type": "dict",
+                    "schema": {
+                        "position": {"required": True, "type": "number"},
+                        "time_hh_mm_ss": {"required": True, "type": "string"},
+                        "ingestion_rate_gb_per_hr": {
+                            "required": True,
+                            "type": "number",
+                        },
+                    },
+                },
+            },
+            "randomness_percentage": {"required": True, "type": "number"},
+        },
+    },
+    "searches": {
+        "required": False,
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "search_type": {"required": True, "type": "string"},
+                "probability": {"required": True, "type": "number"},
+                "cpu_load_percent": {"required": True, "type": "number"},
+                "memory_load_percent": {"required": True, "type": "number"},
+            },
+        },
+    },
+}
