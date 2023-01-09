75f12e79fd422590b8c1425d17b187f76d83d8e5
Akhil-Nair-ML <akhil.nair@maplelabs.com>
2023-01-09 15:57:38 +0530

    Logic changed for fetching cluster Current
    Changed the return type from cluster to clusterDynamic.

--- cluster/cluster_level_parameters.go
+++ cluster/cluster_level_parameters.go
@@ -272,13 +272,12 @@ func GetClusterCount(metricName string, decisonPeriod int, limit float32) (Metri
 //		It will return the current cluster status.
 //
 // Return:
-//		Return populated Cluster struct.
+//		Return populated ClusterDynamic struct.
 
-func GetClusterCurrent() Cluster {
-	var clusterCurrent Cluster
-	var clusterStats map[string]string
+func GetClusterCurrent() ClusterDynamic {
+	var clusterStats ClusterDynamic
 
-	url := fmt.Sprintf("http://localhost:5000/stats/current/status")
+	url := fmt.Sprintf("http://localhost:5000/stats/current")
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
@@ -294,9 +293,8 @@ func GetClusterCurrent() Cluster {
 		log.Panic.Println(err)
 		panic(err)
 	}
-	clusterCurrent.ClusterDynamic.ClusterStatus = clusterStats["current"]
-	log.Debug.Println(clusterCurrent)
-	return clusterCurrent
+	log.Debug.Println(clusterStats)
+	return clusterStats
 }
 
 // Input:
