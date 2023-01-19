69fb73c698d5a8708b769bb8bc5d3ed176c1e059
Shrinidhi-P-ML <Shrinidhi.P@maplelabs.com>
2023-01-19 12:29:08 +0530

    Updated client name

--- fetchmetrics/fetchMetrics.go
+++ fetchmetrics/fetchMetrics.go
@@ -3,42 +3,24 @@ package fetchmetrics
 import (
 	"context"
 	"fmt"
-	"log"
+
+	"scaling_manager/utils"
 
 	opensearch "github.com/opensearch-project/opensearch-go"
-	"scaling_manager/utils"
 )
 
-//Descriptions: Fetch metrics will create an opensearch client and fetches the node and cluster level details and 
-//indexes them into elasticesearch. It also deletes documents that are older than 72 hours
-func FetchMetrics() {
-	ctx := context.Background()
+var ctx = context.Background()
 
-	//create a configuration that is to be passed while creating the client 
-	cfg := opensearch.Config{
-		Addresses: []string{
-			"http://localhost:9200",
-		},
-	}
-
-	//create the client using the configuration
-	esClient, err := opensearch.NewClient(cfg)
-	if err != nil {
-		fmt.Println("Elasticsearch connection error:", err)
-	}
-	res, err := esClient.Info()
-	if err != nil {
-		log.Fatalf("client.Info() ERROR:", err)
-	}
-	fmt.Println("Response: ", res)
-
-	//check if current node is the master node and update the cluster stats if it is master 
-	if utils.CheckIfMaster(esClient, ctx) {  
+// Descriptions: Fetch metrics will create an opensearch client and fetches the node and cluster level details and
+// indexes them into elasticesearch. It also deletes documents that are older than 72 hours
+func FetchMetrics(osClient *opensearch.Client) {
+	//check if current node is the master node and update the cluster stats if it is master
+	if utils.CheckIfMaster(osClient, ctx) {
 		fmt.Println("It's the master node")
-		IndexClusterHealth(esClient, ctx)
+		IndexClusterHealth(osClient, ctx)
 	}
 	//Index the the node stats
-	IndexNodeStats(esClient, ctx)
+	//IndexNodeStats(osClient, ctx)
 	//Purge documents from elasticsearch index that are older than 72 hours
-	DeleteOldDocs(esClient,ctx) 
+	DeleteOldDocs(osClient, ctx)
 }
